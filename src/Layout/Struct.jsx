98271e54aec9402337fa485eccfa62055f59d53a
Khushi <kushibansal047@gmail.com>
2025-07-02 00:38:31 +0530

    responsive added

--- src/Layout/Struct.jsx
+++ src/Layout/Struct.jsx
@@ -3,7 +3,6 @@ import { Task } from "../components/Task";
 import { Prodmo } from "../components/Prodmo";
 import { Quote } from "../components/Quote";
 import { AISuggestions } from "../components/AISuggestions";
-// import { HabitTracker } from "../components/HabitTracker";
 
 export const Struct = () => {
   return (
@@ -13,17 +12,28 @@ export const Struct = () => {
         <FaUser />
       </nav>
 
-      <div className="bg-black min-h-screen overflow-hidden text-white p-6 grid grid-cols-3 gap-3">
-
-        <div className="bg-[#1e293b] rounded-xl p-4"><Task /></div>
-
-        <div className="bg-[#1e293b] rounded-xl p-4"> <Prodmo /></div>
-
-        <div className="bg-[#1e293b]  row-span-2 rounded-xl p-4 text-center flex flex-col justify-center"><Quote /></div>
-
-        <div className="bg-[#1e293b] rounded-xl p-3 col-span-2"><AISuggestions /></div>
+      <div className="bg-black min-h-screen text-white p-6 grid grid-cols-1 gap-4 md:grid-cols-3">
 
      
+        <div className="bg-[#1e293b] rounded-xl p-4">
+          <Task />
+        </div>
+
+       
+        <div className="bg-[#1e293b] rounded-xl p-4">
+          <Prodmo />
+        </div>
+
+       
+        <div className="bg-[#1e293b] rounded-xl p-4 text-center flex flex-col justify-center md:row-span-2">
+          <Quote />
+        </div>
+
+    
+        <div className="bg-[#1e293b] rounded-xl p-3 md:col-span-2">
+          <AISuggestions />
+        </div>
+        
       </div>
     </>
   );
